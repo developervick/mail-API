876a57c31dc8b771bf08f5a1229a785aca1b6374
developervick@gmail.com <developervick@gmail.com>
2023-03-26 08:32:01 +0530

    backend commit

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#display-button').addEventListener('click', display_mail(10));
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -23,13 +24,15 @@ function compose_email() {
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
 
-  document.querySelector("#compose-form").addEventListener("submit", () => {send_mail()})
+  document.querySelector("#submit").addEventListener('click', (event) => { 
+    
+    send_mail(event) }, false)
 
 }
 
-// displaying diffrent categry  of emails 
+// displaying diffrent category  of emails  ** working perfectly fine
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -37,41 +40,86 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  fetch('emails/'+mailbox)
+  .then(response => {
+    console.log(response)
+    return response.json()})
+  .then(result => {
+    console.log(result)
+    listMail(result)
+  });
 }
 
-function send_mail(){
-
+ 
+function send_mail(event){
+  
   //Accessing form values and submitting it
-  form = document.querySelector("#compose-form")
-  recipients = form[1].value
-  subject = form[2].value
-  body = form[3].value
-  
-  console.log(recipients, subject, body)
+  form = document.querySelector("#compose-form");
+  recipients = form[1].value;
+  subject = form[2].value;
+  body = form[3].value;
+
   // sending mail to user and alerting errors errors
-  
   fetch("/emails", {
     method:"POST", 
     body: JSON.stringify({
-      recipients: "pukku@gmail.com",
-      subject: "test 1",
-      body: "body 1",
+      recipients: recipients,
+      subject: subject,
+      body: body,
     })
-  })
-  .then(res => { 
-    console.log(res.json())
-    return res.json()})  
-  .then(results => {    
-     if (results.status == 201 ){
-      alert(results.message+" mail sent")
+  })  
+  .then(response => {   
+     if (response.ok == true ){
       load_mailbox('sent')
      }
-     alert(results.message+ "mail not sent")
-  })
-  .catch(err => { alert(err)})
+    })
+  .catch(err => { console.log(err)});
+
+  event.preventDefault();
 
 }
 
-function show_mails(){}
+function listMail (mails){
 
-function list_mail(){}
+  mail_List = document.querySelector('#mail-list');
+  console.log(mail_List+"maillist");
+  
+  
+  mails.forEach((mail) => { 
+    console.log(mail.subject);
+    
+    list_Mail = document.createElement('li');
+    list_Mail.innerHTML = mail.subject;
+    mail_List.appendChild(list_Mail);
+    
+  }); 
+
+}
+
+function display_mail(email_id){
+
+  console.log(" called display mail")
+
+  document.querySelector("#emails-view").style.display == 'none';
+  document.querySelector('#display-mail').style.display == 'block';
+
+  fetch('emails/'+email_id)
+  .then(response => { 
+    if (response.ok){
+      return response.json();
+    }
+    else {
+      console.log("some error occured");
+    }
+    })
+  .then(result => {
+    console.log(result.body+" body");
+    console.log(result.subject+" subject");
+    document.querySelector("#mail-body").textContent == result.body;
+    document.querySelector("#from").innerHTML == result.sender;
+
+  })
+  .catch(err => {consol.log(err)});
+}
+
+
--- mail/project3/settings.py
+++ mail/project3/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
-STATIC_ROOT = "/mail/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
-#TATICFILES_DIRS = [ os.path.join(BASE_DIR, "mail", "mail", "static") ]
-
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
